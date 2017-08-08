5507b3993904c2accf81df2c15fe0c5b93a5d39d
Jibran Kalia <jkalia@student.42.us.org>
2017-08-08 10:47:39 -0700

    Working on ex11

--- ex08/isFilled.c
+++ ex08/isFilled.c
@@ -3,10 +3,5 @@
 
 int isFilled(unsigned int parkingRow)
 {
-	unsigned int tmp = parkingRow;
-	unsigned int msb = 0;
-	while (tmp >>= 1)
-		++msb;
-	tmp = (1 << (msb + 1)) - 1;
-	return ((parkingRow ^ tmp) ? 0 : 1);
+	return !(parkingRow & (parkingRow + 1));
 }
