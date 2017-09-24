9aa72e884d3ddd1baf1b5d4719d68d49e924396e
Jibran Kalia <jibran.kalia@gmail.com>
2017-09-24 16:17:08 -0700

    Able to read file without error

---
+++ day00/ex00/searchPrice.py
@@ -0,0 +1,25 @@
+class art:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+    def __str__(self):
+        return self.name + "  " + str(self.price)
+
+if __name__ == "__main__":
+    with open("art.txt", "r") as file:
+        array = []
+        error = False
+        for line in file:
+            try: 
+                if error is True:
+                    line = tmp + line
+                name, number = line.split(" => ")
+            except ValueError: 
+                error = True
+                tmp = line.strip()
+            else:
+                error = False
+                array.append(art(name, int(number)))
+
+    #for i in array:
+    #    print(i)
